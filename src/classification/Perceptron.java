6952ed9aba9c04d85bfaaffdfca73aff35a82834
otamot <tomo_otamot@yahoo.co.jp>
2016-07-27 13:35:17 +0900

    edit classification source code

--- src/classification/Perceptron.java
+++ src/classification/Perceptron.java
@@ -2,86 +2,117 @@ package classification;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import lib.Matrix;
 
+
+/**
+ * パーセプトロンによる分類を行う
+ * @author otamot
+ * @since 2016/07/27
+ * @version 1.0
+ */
 public class Perceptron {
-	private final int N = 10000;
-	double[][] x;
-	double[][] t;
+	/**
+	 * データ群の特徴量を保持するMatrix型の変数
+	 */
+	private Matrix x;
+	/**
+	 * データ群の正解ラベルを保持するMatrix型の変数
+	 */
+	private Matrix t;
+	/**
+	 * パーセプトロンによって求める重みを保持する変数
+	 */
+	private Matrix w;
 
-	public Perceptron(){
-		setX();
-		setT();
-	}
-
-	private void setX(){
-		Random rnd = new Random();
-		x = new double[N][2];
-		for(int i = 0; i < x.length;i++){
-			for(int j = 0; j < x[i].length; j++){
-				x[i][j] = rnd.nextGaussian();
-			}
-		}
-	}
-
-
-	private double h(double x, double y){
-		return 5 * x + 3 * y - 1;
-	}
-
-	private void setT(){
-		t = new double[N][1];
-		for(int i = 0; i < x.length; i++){
-			t[i][0] = (h(x[i][0], x[i][1]) > 0)?1:-1;
-		}
-	}
-
-	private Matrix phi(double x, double y){
-		double[][] phi_i = new double[1][3];
-		phi_i[0][0] = x;
-		phi_i[0][1] = y;
-		phi_i[0][2] = 1.0;
-		return new Matrix(phi_i);
+	/**
+	 * コンストラクタ
+	 * @param x 特徴量
+	 * @param t 正解ラベル。1か-1のラベルが入っていることを前提とする。
+	 */
+	public Perceptron(double[][] x, double[][] t){
+		this.x = new Matrix(x);
+		this.t = new Matrix(t);
+		this.w = new Matrix(this.x.col_n()+1,1);
 	}
 
 
+
+	/**
+	 * 基底関数。
+	 * Φ(x) = ΣΦ(xi)= 1 + x1 + x2 +...+ xn
+	 * となるような基底関数。
+	 * @param x_i
+	 * @return
+	 */
+	private Matrix phi(double[] x_i){
+		double[][] phi = new double[1][x_i.length+1];
+		for(int i = 0; i < x_i.length; i++)
+			phi[0][i] = x_i[i];
+		phi[0][phi[0].length-1] = 1.0;		
+		return new Matrix(phi);
+	}
+
+
+	/**
+	 * xが正なら1,0なら0,負なら-1を返す関数
+	 * @param x 引数
+	 * @return 符号に対応したint値
+	 */
 	private int sign(double x){
 		return (x>0)?1:(x==0)?0:-1;
 	}
-	private void cal(){
-		Matrix w = new Matrix(3,1);
+	
+	/**
+	 * パーセプトロンの学習を行う。
+	 */
+	public void cal(){
+		//データの学習する順序をシャッフルするためのリスト
 		List<Integer> list = new ArrayList<>();
-		for(int i= 0; i < N; i++){
+		for(int i= 0; i < x.row_n(); i++){
 			list.add(i);
 		}
 		while(true){
+			//学習順序のシャッフル
 			Collections.shuffle(list);
+			//分類予想が外れた回数を保持する変数
 			int misses = 0;
-
-			for(int n:list){
-				double x_n = x[n][0];
-				double y_n = x[n][1];
-				double t_n = t[n][0];
-				int predict = sign(phi(x_n,y_n).mul(w).toArray()[0][0]);
-				if((double)predict != t_n){
-					w = w.add(phi(x_n, y_n).scalar(t_n).t());
+			for(int n:list){//データを1つずつ取り出して学習。
+				//n癌目のデータの正解ラベルを取り出す。
+				double t_n = t.getCell(n, 0);
+				//n番目の特徴量を基底関数に入れる。
+				Matrix feature = phi(x.getRow(n).toArray()[0]);
+				//n番目のデータの所属ラベルを予測する
+				int predict = sign(feature.mul(w).toArray()[0][0]);
+				if((double)predict != t_n){//所属ラベルと正解ラベルが不一致だった時
+					//重みの更新を行う。
+					w = w.add((feature).scalar(t_n).t());
+					//分類予想が外れたので+1する
 					misses += 1;
 				}
 			}
-			System.out.println();
-			w.printMatrix();
+			//分類予想が全て当たったら学習の終了
 			if(misses == 0)
 				break;
 		}
 	}
-
-
-	//テスト用メソッド
-	public static void main(String [] args){
-		Perceptron per= new Perceptron();
-		per.cal();
+	
+	/**
+	 * 新しいデータに般化させる
+	 * 新しい特徴量を入れた時のそのデータが正解に分類されたら1,不正解に分類されたら-1を返す。
+	 * @param feature 特徴量
+	 * @return 分類されたラベル
+	 */
+	public int fit(double[] feature){
+		return sign(phi(feature).mul(w).toArray()[0][0]);
 	}
-
+	
+	/**
+	 * パーセプトロンによって求めた重みwをdoubleの1次元配列にて返す。
+	 * @return 重みwの1次元配列
+	 */
+	public double[] getWeight(){
+		return (w.t()).getRow(0).toArray()[0];
+	}
 }
 
--- src/classification/PerceptronMain.java
+++ src/classification/PerceptronMain.java
@@ -1,16 +1,17 @@
+package classification;
+
 import java.util.Random;
 
-import classification.LogisticRegression;
-
-public class LogisticRegressionMain {
+//Perceptronクラスの実行確認用のクラス
+public class PerceptronMain {
 	
+
 	private static double h(double x, double y){
 		return 5 * x + 3 * y - 1;
 	}
 
 	
 	public static void main(String[] args){
-		
 		int N = 10000;
 		Random rnd = new Random();
 		double[][] x = new double[N][2];
@@ -22,18 +23,16 @@ public class LogisticRegressionMain {
 		
 		double[][] t = new double[N][1];
 		for(int i = 0; i < x.length; i++){
-			t[i][0] = (h(x[i][0], x[i][1]) > 0)?1:0;
+			t[i][0] = (h(x[i][0], x[i][1]) > 0)?1:-1;
 		}
 		
-		LogisticRegression lr = new LogisticRegression(x,t);
-		lr.cal();
+		Perceptron p = new Perceptron(x,t);
+		p.cal();
 		
-		double[][] feature = {{2,-3},{5.0/8.0,3.0/8.0},{0.0,0.0}};
-		System.out.println(lr.fit(feature[0]));
-		System.out.println(lr.fit(feature[1]));
-		System.out.println(lr.fit(feature[2]));
-		
-		
+		double[][] feature = {{2,3},{5.0/8.0,3.0/8.0},{0.0,0.0}};
+		System.out.println(p.fit(feature[0]));
+		System.out.println(p.fit(feature[1]));
+		System.out.println(p.fit(feature[2]));
 		
 	}
 
